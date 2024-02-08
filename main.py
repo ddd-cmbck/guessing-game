dab616f8f3e803c7d2e584a94acb78713a834236
Dim4k <dd.darknet.tt@gmail.com>
2024-02-08 00:05:04 +0000

    added basic logic

--- main.py
+++ main.py
@@ -1,3 +1,50 @@
-print('zalupa')
+from argparse import ArgumentParser
+import random
 
-print('zalupaaa' * 300)
+parser = ArgumentParser()
+
+MIN_VALUE = 0
+MAX_VALUE = 100
+
+parser.add_argument('-g', '--guess', type=int, help='Enter number between given range ', )
+
+
+def generate_number(min_range=0, max_range=100):
+    rand_num = random.randint(min_range, max_range)
+    return rand_num
+
+
+def user_int(prompt):
+    global user_num
+    while True:
+        try:
+            user_num = int(input(prompt))
+            break
+
+        except ValueError:
+            print('Enter correct value(int)')
+    return user_num
+
+
+def check_input(random_num: int, user_num: int):
+    if user_num > random_num:
+        print('Enter smaller number')
+        return False
+    elif user_num < random_num:
+        print('Enter bigger number')
+        return False
+    else:
+        print('You guessed correctly')
+        return True
+
+
+def main():
+    check = False
+    random_num = generate_number(MIN_VALUE, MAX_VALUE)
+    while not check:
+        user_num = user_int(f'Try to guess number between {MIN_VALUE} and {MAX_VALUE}: >> ')
+        check = check_input(random_num, user_num)
+
+
+if __name__ == '__main__':
+    main()
